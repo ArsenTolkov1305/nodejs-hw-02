37a095c61e5a4cdb75ff5d93502208587fabd88e
Arsen Tolkov <formycomputer1305@gmail.com>
2025-06-08 00:32:05 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -33,6 +33,8 @@ export const setupServer = () => {
     const contacts = await getAllContacts();
 
     res.status(200).json({
+      status: 200,
+      message: 'Successfully found contacts!',
       data: contacts,
     });
   });
@@ -49,6 +51,8 @@ export const setupServer = () => {
     }
 
     res.status(200).json({
+      status: 200,
+      message: `Successfully found contact with id ${contactId}!`,
       data: contact,
     });
   });
