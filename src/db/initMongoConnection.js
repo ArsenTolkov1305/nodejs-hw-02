ba1f4f2908ef61b007ee66b0d55e6155d9f488e0
Arsen Tolkov <formycomputer1305@gmail.com>
2025-06-08 00:23:37 +0300

    update

---
+++ src/db/initMongoConnection.js
@@ -0,0 +1,62 @@
+import mongoose from 'mongoose';
+import { model, Schema } from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    isFavorite: {
+      type: Boolean,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      default: 'personal',
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+export const contactsCollection = model('contacts', contactsSchema);
+
+export const initMongoDB = async () => {
+  try {
+    const user = process.env.MONGODB_USER;
+    const pwd = process.env.MONGODB_PASSWORD;
+    const url = process.env.MONGODB_URL;
+    const db = process.env.MONGODB_DB;
+
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`,
+    );
+    console.log('Mongo connection successfully established!');
+  } catch (e) {
+    console.log('Error while setting up mongo connection', e);
+    throw e;
+  }
+};
+
+export const getAllContacts = async () => {
+  const contacts = await contactsCollection.find();
+  return contacts;
+};
+
+export const getContactById = async (studentId) => {
+  const contact = await contactsCollection.findById(studentId);
+  return contact;
+};
